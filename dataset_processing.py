c88ca2e2f8e76ccd5a3037bed0db2a4a11889b2b
silvimica <mayagoloburda@gmail.com>
2025-04-08 17:35:37 +0400

    Finalized code

--- dataset_processing.py
+++ dataset_processing.py
@@ -1,13 +1,13 @@
 
 
-def get_columns(type ='diabetes'):
-    if type=='diabetes':
+def get_columns(type ='diab'):
+    if type=='diab':
         columns = ['DID040','DIQ159', 'DIQ160', 'DIQ180','DIQ050','DID060','DIQ060U', 'DIQ065', 'DIQ070']
         diagnosis_column = 'DIQ010'
-    if type=='cvd':
-        columns = ['MCQ160b','MCQ160d', 'MCQ160e', 'MCQ160f']
-        diagnosis_column = 'MCQ160c'
-    if type=='kidney_conditions':
+    elif type=='cvd':
+        columns = ['MCQ160B','MCQ160D', 'MCQ160E', 'MCQ160F','MCQ160C']
+        diagnosis_column = 'MCQ160C'
+    elif type=='kidney_conditions':
         columns = ['KIQ025','KIQ005', 'KIQ010', 'KIQ042', 'KIQ044', 'KIQ048A', 'KIQ052', 'KIQ481']
         diagnosis_column = 'KIQ022'
     else:
